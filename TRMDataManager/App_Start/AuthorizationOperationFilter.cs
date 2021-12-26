7d9f18ef2ec68fd92061cf2ca0147db6f422b9cd
Andrea Konomi <andreakonomi23@gmail.com>
2021-12-27 00:43:59 +0100

    Added and Configured Swagger Documentation for API
    Added Swashbuckle/Swagger to the API
    for documentation and testing.

---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    /// <summary>
+    /// Allows an extra parameter on every single method to be aple to pass the authorization token neccessary to get back data.
+    /// </summary>
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)   //get all request case, no parameters
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
