a6e747171bdd8921a232b5c58a0c86152b7118ea
Andrea Konomi <andreakonomi23@gmail.com>
2021-12-27 12:26:40 +0100

    Added Wpf project with MVVM w/ CalibrunMicro

---
+++ TRMWPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // sets the startup "form" as the ShellViewModel. This is the setup for Caliburn Micro
+            // very similar actually to the startup code of the windows forms
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
