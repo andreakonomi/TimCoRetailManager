035eb57388f114ead61d381c49e36469c7ce7d30
Andrea Konomi <andreakonomi23@gmail.com>
2021-12-31 23:05:56 +0100

    register db tables

---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[SaleId] INT NOT NULL,
+	[ProductId] int not null,
+	[Quantity] int not null default 1,
+	[PurchasePrice] money not null,
+	[Tax] money not null DEFAULT 0
+)
