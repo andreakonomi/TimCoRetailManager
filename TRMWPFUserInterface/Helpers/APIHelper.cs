1030789690fa64efb5e845dab71ef3688cc66241
Andrea Konomi <andreakonomi23@gmail.com>
2022-01-01 21:11:20 +0100

    set apiHelper, check authentication with api on login button

---
+++ TRMWPFUserInterface/Helpers/APIHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Models;
+
+namespace TRMDesktopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+        private HttpClient apiClient;
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            //loads the api url from the app.config
+            string api = ConfigurationManager.AppSettings["api"];
+
+            apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri(api);
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+
+            // sets the accept format for responses to be json, accept header specifies that
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /// <summary>
+        /// Authenticates the user credentials.
+        /// </summary>
+        /// <param name="username">The username to auth.</param>
+        /// <param name="password">The password to auth</param>
+        /// <returns>An authenticated user model with the username and authentication token, or throws exc if not valid.</returns>
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            using (HttpResponseMessage response = await apiClient.PostAsync("/Token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
