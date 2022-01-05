e393982f0e3f57f7ea78649ba61334f25bbf423b
Andrea Konomi <andreakonomi23@gmail.com>
2022-01-05 01:26:54 +0100

    Add Sales Page
    Adds the sales page XAML and the supporting View Model. Just temp setup out of the Vm, doesnt add actual logic.

---
+++ TRMWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set 
+            { 
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set 
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal => "$0.00";  // add calculation
+        public string Total => "$0.00";  // add calculation
+        public string Tax => "$0.00";  // add calculation
+
+        public bool CanAddToCart => true;   // add check sth is selected, item quantity is okay
+
+        public void AddToCart()
+        {
+             
+        }
+
+        public bool CanRemoveFromCart => true;  // add check
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut => true;  // add check
+
+        public void CheckOut()
+        {
+
+        }
+
+    }
+}
