57f0ad2aff6c2d97491e77a65dcdc34474103636
Andrea Konomi <andreakonomi23@gmail.com>
2022-01-09 02:19:57 +0100

    Add foreign key relationships
    adds Fk Relationships on tables.

--- TRMData/dbo/Tables/SaleDetail.sql
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -5,5 +5,7 @@
 	[ProductId] int not null,
 	[Quantity] int not null default 1,
 	[PurchasePrice] money not null,
-	[Tax] money not null DEFAULT 0
+	[Tax] money not null DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id)
 )
